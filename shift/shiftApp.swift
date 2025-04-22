1d5a01dc6a36998b52416a41b521062f0c030f12
Christian Cattaneo <christian.cattaneo@gauntletai.com>
2025-04-21 22:54:55 -0500

    Initial Commit

---
+++ shift/shiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  shiftApp.swift
+//  shift
+//
+//  Created by Christian Cattaneo on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct shiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shiftTests/shiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  shiftTests.swift
+//  shiftTests
+//
+//  Created by Christian Cattaneo on 4/21/25.
+//
+
+import Testing
+@testable import shift
+
+struct shiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
